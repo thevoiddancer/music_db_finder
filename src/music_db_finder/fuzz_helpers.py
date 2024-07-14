af72fd0c88ecafda871a3951b8340d67709e96e9
Tomislav Nazifovic <tomislav.nazifovic@gmail.com>
2024-07-14 18:59:16 +0200

    find band by album

--- src/music_db_finder/fuzz_helpers.py
+++ src/music_db_finder/fuzz_helpers.py
@@ -2,18 +2,29 @@
 import discogs_client as DiscogsClient
 from rapidfuzz import fuzz as r_fuzz
 
-
 def get_discogs_name(object):
     if type(object) == DiscogsClient.models.Artist:
-        object = object.name
+        name = object.name
     elif type(object) in [DiscogsClient.models.Master, DiscogsClient.models.Release]:
-        object = object.title
-    return object.lower()
+        name = object.title.split(' - ')[1]
+    return name
+    # return getattr(object, 'name', getattr(getattr(object, 'master', getattr(object, 'main_release', None)), 'title', 'none'))
+
+def get_discogs_name_as_str(object):
+    # if type(object) == DiscogsClient.models.Artist:
+    #     object = object.name
+    # elif type(object) in [DiscogsClient.models.Master, DiscogsClient.models.Release]:
+    #     object = object.title
+    # return object.lower()
+    if type(object) == str:
+        return object.lower()
+    else:
+        return get_discogs_name(object)
 
 
 def obj_ratio(a, b, scorer=r_fuzz.token_sort_ratio, *args, **kwargs):
     # print(type(a))
     # print(type(b))
-    a = get_discogs_name(a)
-    b = get_discogs_name(b)
+    a = get_discogs_name_as_str(a)
+    b = get_discogs_name_as_str(b)
     return scorer(a, b, *args, **kwargs)
