13b5e6d89c1de653d82d9ca9af74fbd2654c5897
Tomislav Nazifović <tomislav.nazifovic@gmail.com>
2024-07-19 21:37:23 +0200

    Universality starts

--- src/music_db_finder/fuzz_helpers.py
+++ src/music_db_finder/fuzz_helpers.py
@@ -10,23 +10,18 @@ def get_discogs_name(object):
         if object.data['artist'] + ' - ' in name:
             name = name.split(' - ')[1]
     return name
-    # return getattr(object, 'name', getattr(getattr(object, 'master', getattr(object, 'main_release', None)), 'title', 'none'))
 
-def get_discogs_name_as_str(object):
-    # if type(object) == DiscogsClient.models.Artist:
-    #     object = object.name
-    # elif type(object) in [DiscogsClient.models.Master, DiscogsClient.models.Release]:
-    #     object = object.title
-    # return object.lower()
+
+def get_name_as_str(object):
     if type(object) == str:
         return object.lower()
+    if type(object) == dict:
+        return object['name']
     else:
         return get_discogs_name(object).lower()
 
 
 def obj_ratio(a, b, scorer=r_fuzz.token_sort_ratio, *args, **kwargs):
-    # print(type(a))
-    # print(type(b))
-    a = get_discogs_name_as_str(a)
-    b = get_discogs_name_as_str(b)
+    a = get_name_as_str(a)
+    b = get_name_as_str(b)
     return scorer(a, b, *args, **kwargs)
